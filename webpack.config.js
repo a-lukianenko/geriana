c971597ba9aae7da0339ecf9df02460a014502fd
Andrii Lukianenko <lukyanenko.andrew@gmail.com>
2020-11-03 16:20:23 +0300

    further styles added

--- webpack.config.js
+++ webpack.config.js
@@ -5,60 +5,56 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].[fullhash].bundle.js",
+    filename: "[name].[fullhash].js",
   },
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: path.resolve(__dirname, "/node_modules"),
-      //   use: [
-      //     {
-      //       loader: "babel-loader",
-      //       options: {
-      //         presets: ["@babel/preset-env"],
-      //       },
-      //     },
-      //   ],
-      // },
       {
         test: /\.scss$/,
         use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "postcss-loader" },
+          { loader: "resolve-url-loader" },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+              // sourceMapContents: false,
+            },
+          },
         ],
       },
+      // {
+      //   test: /\.html$/,
+      //   use: ["html-loader"],
+      // },
       {
-        test: /\.html$/,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.(jpe?g|png|svg)$/i,
+        test: /\.(jpe?g|png|svg)$/,
         use: [
           {
             loader: "file-loader",
             options: {
-              name: "[name].[ext]",
-              outputPath: "img/",
-              publicPath: "img/",
+              name: "img/[name].[hash].[ext]",
+              outputPath: "img",
+              // publicPath: "img",
             },
           },
         ],
       },
       {
-        test: /\.(ttf|woff)$/,
+        test: /\.(ttf)$/,
         use: [
           {
             loader: "file-loader",
             options: {
               name: "[name].[ext]",
-              outputPath: "fonts/",
-              publicPath: "fonts/",
+              outputPath: "fonts",
+              // publicPath: "fonts",
             },
           },
         ],
@@ -67,10 +63,10 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({ patterns: [{ from: "src/img", to: "img" }] }),
-    new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css",
-    }),
+    // new CopyWebpackPlugin({ patterns: [{ from: "src/img", to: "img" }] }),
+    // new MiniCssExtractPlugin({
+    //   filename: "[name].[contenthash].css",
+    // }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
