446eb840d63ade738005e67fe7b801a9b79e47d5
Andrii Lukianenko <lukyanenko.andrew@gmail.com>
2020-11-03 10:11:43 +0300

    removed unnecessary files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import $ from "jquery";
+import "./scss/main.scss";
 
 $(function () {
   console.log("Hello");
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,79 @@
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "[name].[fullhash].bundle.js",
   },
   module: {
     rules: [
+      // {
+      //   test: /\.js$/,
+      //   exclude: path.resolve(__dirname, "/node_modules"),
+      //   use: [
+      //     {
+      //       loader: "babel-loader",
+      //       options: {
+      //         presets: ["@babel/preset-env"],
+      //       },
+      //     },
+      //   ],
+      // },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(jpe?g|png|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img/",
+              publicPath: "img/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|woff)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+              publicPath: "fonts/",
+            },
+          },
+        ],
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({ patterns: [{ from: "src/img", to: "img" }] }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
 };
