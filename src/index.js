71134e7ee20bce9d71b36cbb77f60158129ddb89
Andrii Lukianenko <lukyanenko.andrew@gmail.com>
2020-11-10 12:30:58 +0300

    added modular html

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 // import "./scss/main.scss";
 
 function toggleMenu() {
-  if (window.innerWidth <= 768) {
-    const btn = document.querySelector(".hamburger");
-    const menu = document.querySelector(".menu-hidden");
-    btn.onclick = function () {
+  const btn = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu-hidden");
+  btn.onclick = function () {
+    if (window.innerWidth <= 768) {
       if (menu.classList.contains("unhide")) {
         menu.classList.remove("unhide");
       } else {
         menu.classList.add("unhide");
       }
-    };
+    }
+  };
+}
+
+function myFunction() {
+  const nav = document.getElementById("nav");
+  const logo = document.getElementById("logo");
+  const src1 = "./img/logo.png";
+  const src2 = "./img/logo_2.png";
+
+  const sticky = nav.offsetTop;
+  if (window.pageYOffset > sticky) {
+    nav.classList.add("sticky");
+    logo.src = src2;
+  } else {
+    nav.classList.remove("sticky");
+    logo.src = src1;
   }
 }
 
 window.onload = function () {
   toggleMenu();
+};
+
+window.onscroll = function () {
+  myFunction();
 };
